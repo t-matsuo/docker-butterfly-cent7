306d830513d849bdd88d01e699b76f9e8b42f054
Takatoshi MATSUO <matsuo.tak@gmail.com>
2020-06-28 20:56:01 +0900

    exec args if it's specified

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ "$1" != "" ]; then
+    exec $@
+fi
+
 if [ ! -f /etc/butterfly-init-done ]; then
     if [ "$PASSWORD" = "" ]; then
         PASSWORD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1`
